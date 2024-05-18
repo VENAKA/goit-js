8c7cf18ae4e894a43956b2184b0673109bd6e908
VENAKA <125134919+VENAKA@users.noreply.github.com>
2024-05-18 16:06:13 +0300

    10

--- skript.js
+++ skript.js
@@ -118,3 +118,63 @@ chuVaki.forEach(user => {
     sum += user.balance
 })
 console.log(sum)
+
+function getByFriend(user , friendo){
+    return user.filter( fr => fr.friends === friendo)
+}
+
+console.log(chuVaki, 'Briana Decker')
+
+let sortName = chuVaki
+.sort((a,b) => (b.friends).length - (a.friends).length)
+.map(val => val.name)
+console.log(sortName)
+
+
+
+/* let players = [
+    { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+    { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+    { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+    { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+    { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+    ] */
+
+
+/* 
+    let playersTime = players.reduce((sum,hor) => sum + hor.timePlayed,0)
+    console.log(playersTime) */
+/* 
+    let imena = ['Alex','Vena','Gleb','Greg','Taras','vlad']
+    let arrName = players.sort((a,b) => a.localeCompare(b)) */
+
+
+/*     let arry = [,456,6,7,34,6,65,3,79,100,86,34]
+
+    let sum = arry
+    .filter(val => val % 2 !== 0 )
+    .map(lol => lol * 2)
+    console.log(sum)
+ */
+
+/*     const numbers = [1, 2, 3, 4, 5];
+
+    let sum = numbers.reduce((a,b) => (a + b)/5,0)
+    console.log(sum) */
+
+/*     let items = [
+        { name: 'Item A', popularity: 5 },
+        { name: 'Item C', popularity: 2 },
+        { name: 'Item D', popularity: 3 },
+        { name: 'Item B', popularity: 5 }
+        ]
+
+        let popul = items.sort((a,b) => {
+            if(b.popularity === a.popularity){
+                return b.name - a.name
+            }
+            return b.popularity - a.popularity
+        })
+        console.log(popul) */
+
+
